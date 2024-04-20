0aa016473e21ebc2892fc372db37f490d1071bbb
Nicky Lim <nickylim.p@gmail.com>
2024-04-21 02:29:45 +0800

    Update plugins and add tailwind and rust config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,26 @@ return {
         emmet_language_server = {
           filetypes = { "html", "css", "typescriptreact", "heex" },
         },
+        tailwindcss = {
+          filetypes = { "html", "heex" },
+        },
+      },
+      setup = {
+        rust_analyzer = function(_, _)
+          vim.g.rustaceanvim = {
+            server = {
+              on_attach = function(_, bufnr)
+                vim.keymap.set("n", "<leader>cR", function()
+                  vim.cmd.RustLsp("codeAction")
+                end, { desc = "Code Action", buffer = bufnr })
+                vim.keymap.set("n", "<leader>dr", function()
+                  vim.cmd.RustLsp("debuggables")
+                end, { desc = "Rust Debuggables", buffer = bufnr })
+                vim.lsp.inlay_hint.enable(bufnr)
+              end,
+            },
+          }
+        end,
       },
     },
   },
