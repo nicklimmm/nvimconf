1d4b9b65a46d00f1f0a616a4bce0d6a1cb3bd446
Nicky Lim <nickylim.p@gmail.com>
2024-04-22 21:47:26 +0800

    Remove custom rust lsp config and add gleam

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,23 +11,7 @@ return {
         tailwindcss = {
           filetypes = { "html", "heex" },
         },
-      },
-      setup = {
-        rust_analyzer = function(_, _)
-          vim.g.rustaceanvim = {
-            server = {
-              on_attach = function(_, bufnr)
-                vim.keymap.set("n", "<leader>cR", function()
-                  vim.cmd.RustLsp("codeAction")
-                end, { desc = "Code Action", buffer = bufnr })
-                vim.keymap.set("n", "<leader>dr", function()
-                  vim.cmd.RustLsp("debuggables")
-                end, { desc = "Rust Debuggables", buffer = bufnr })
-                vim.lsp.inlay_hint.enable(bufnr)
-              end,
-            },
-          }
-        end,
+        gleam = {},
       },
     },
   },
