ec0115aa00547110627972a5621c34afb2eea95f
Nicky Lim <nickylim.p@gmail.com>
2024-05-07 13:28:45 +0800

    Add text-case and treesitter config and outline

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,8 @@ return {
         "ocaml",
         "elixir",
         "heex",
+        "gleam",
+        "rust",
       },
     },
   },
